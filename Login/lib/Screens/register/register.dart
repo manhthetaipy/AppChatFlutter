dde4ebc8352cc17a454fb144cf9e18ed337f20e9
Tuoif <101004279+Tuoif@users.noreply.github.com>
2022-04-01 01:20:11 +0700

    upp

---
+++ Login/lib/Screens/register/register.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+import 'package:login_ui/Screens/login/login.dart';
+import 'package:login_ui/components/background.dart';
+
+class RegisterScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
