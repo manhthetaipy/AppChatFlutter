0b794df9c8fe8691ddfe794375edcc4c3f35a5b8
HoainamUda <102743615+HoainamUda@users.noreply.github.com>
2022-04-01 00:44:55 +0700

    up

--- Login/lib/components/background.dart
+++ Login/lib/components/background.dart
@@ -9,3 +9,13 @@ class Background extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     Size size = MediaQuery.of(context).size;
+    return Container(
+      width: double.infinity,
+      height: size.height,
+      child: Stack(
+        alignment: Alignment.center,
+        children: <Widget>[child],
+      ),
+    );
+  }
+}
