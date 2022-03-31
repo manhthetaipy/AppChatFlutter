ab6ab047e1d662cef06a0e7b12c37da2d3292563
The Tai Manh <84488032+ttaidev@users.noreply.github.com>
2022-04-01 01:33:38 +0700

    InputDecoration

--- lib/modules/chat_page.dart
+++ lib/modules/chat_page.dart
@@ -76,3 +76,44 @@ class _ChatPageState extends State<ChatPage> {
                 ),
               ),
             ),
+            Padding(
+              padding: EdgeInsets.only(top: 16, left: 16, right: 16),
+              child: TextField(
+                decoration: InputDecoration(
+                  hintText: "Tìm bạn hẹn hò...",
+                  hintStyle: TextStyle(color: Colors.grey.shade400),
+                  prefixIcon: Icon(
+                    Icons.search,
+                    color: Colors.grey.shade400,
+                    size: 20,
+                  ),
+                  filled: true,
+                  fillColor: Colors.grey.shade100,
+                  contentPadding: EdgeInsets.all(8),
+                  enabledBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(30),
+                      borderSide: BorderSide(color: Colors.grey.shade100)),
+                ),
+              ),
+            ),
+            ListView.builder(
+              itemCount: chatUsers.length,
+              shrinkWrap: true,
+              padding: EdgeInsets.only(top: 16),
+              physics: NeverScrollableScrollPhysics(),
+              itemBuilder: (context, index) {
+                return ChatUsersList(
+                  text: chatUsers[index].text,
+                  secondaryText: chatUsers[index].secondaryText,
+                  image: chatUsers[index].image,
+                  time: chatUsers[index].time,
+                  isMessageRead: (index == 0 || index == 3) ? true : false,
+                );
+              },
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
