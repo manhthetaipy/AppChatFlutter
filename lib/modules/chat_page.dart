69ca08d83c0f78148d280b6f559b9433ec3d517a
The Tai Manh <84488032+ttaidev@users.noreply.github.com>
2022-04-01 01:31:59 +0700

    ChatPage

--- lib/modules/chat_page.dart
+++ lib/modules/chat_page.dart
@@ -1,6 +1,5 @@
 import 'package:chat_app/components/chat.dart';
 import 'package:chat_app/models/chat_users.dart';
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class ChatPage extends StatefulWidget {
@@ -14,95 +13,15 @@ class _ChatPageState extends State<ChatPage> {
         text: "Mạnh Thế Tài",
         secondaryText: "Toi la sinh vien nam 3",
         image: "images/userImage1.jpg",
-        time: "Đang hoạt động")
+        time: "Đang hoạt động"),
+    ChatUsers(
+        text: "Nguyễn Quang Tươi",
+        secondaryText: "Tôi là Tươi",
+        image: "images/userImage2.jpg",
+        time: "1p trước"),
+    ChatUsers(
+        text: "Nam Hoài Nguyễn",
+        secondaryText: "Nam Bịp",
+        image: "images/userImage4.jpg",
+        time: "10p trước"),
   ];
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: SingleChildScrollView(
-        physics: BouncingScrollPhysics(),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: <Widget>[
-            SafeArea(
-              child: Padding(
-                padding: EdgeInsets.only(left: 16, right: 16, top: 10),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: <Widget>[
-                    Text(
-                      "Chats",
-                      style:
-                          TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
-                    ),
-                    Container(
-                      padding:
-                          EdgeInsets.only(left: 8, right: 8, top: 2, bottom: 2),
-                      height: 30,
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(30),
-                        color: Colors.pink[50],
-                      ),
-                      child: Row(
-                        children: <Widget>[
-                          Icon(
-                            Icons.add,
-                            color: Colors.pink,
-                            size: 20,
-                          ),
-                          SizedBox(
-                            width: 2,
-                          ),
-                          Text(
-                            "New",
-                            style: TextStyle(
-                                fontSize: 14, fontWeight: FontWeight.bold),
-                          ),
-                        ],
-                      ),
-                    )
-                  ],
-                ),
-              ),
-            ),
-            Padding(
-              padding: EdgeInsets.only(top: 16, left: 16, right: 16),
-              child: TextField(
-                decoration: InputDecoration(
-                  hintText: "Search...",
-                  hintStyle: TextStyle(color: Colors.grey.shade400),
-                  prefixIcon: Icon(
-                    Icons.search,
-                    color: Colors.grey.shade400,
-                    size: 20,
-                  ),
-                  filled: true,
-                  fillColor: Colors.grey.shade100,
-                  contentPadding: EdgeInsets.all(8),
-                  enabledBorder: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(30),
-                      borderSide: BorderSide(color: Colors.grey.shade100)),
-                ),
-              ),
-            ),
-            ListView.builder(
-              itemCount: chatUsers.length,
-              shrinkWrap: true,
-              padding: EdgeInsets.only(top: 16),
-              physics: NeverScrollableScrollPhysics(),
-              itemBuilder: (context, index) {
-                return ChatUsersList(
-                  text: chatUsers[index].text,
-                  secondaryText: chatUsers[index].secondaryText,
-                  image: chatUsers[index].image,
-                  time: chatUsers[index].time,
-                  isMessageRead: (index == 0 || index == 3) ? true : false,
-                );
-              },
-            ),
-          ],
-        ),
-      ),
-    );
-  }
-}
